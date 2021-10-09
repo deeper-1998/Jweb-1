77c343ae2365498d3edf8751936e107b6ad0a567
deeper-1998 <79179091+deeper-1998@users.noreply.github.com>
2021-10-09 15:52:02 +0900

    Add files via upload

--- exhidbition/js/exhidbition.js
+++ exhidbition/js/exhidbition.js
@@ -28,20 +28,59 @@ function orgOpen() { // 관련기관 펼치고닫기
         orgBtn.style.transition = "border-radius 0s 0.4s";
     }
 }
+
+
 const slideList = document.querySelector("#slideList");
 const slideListImg = slideList.children;
+const slideBg = document.querySelector(".slideBanner");
+const slideStick = document.querySelector(".slideStick");
+const slideBtn = document.querySelector(".slideBtn");
+let stickMove = 0;
+const stickMoveAvg = (150 - 40) / (slideListImg.length - 1);
+
 slideListImg[0].style.width = "500px";
 slideListImg[0].style.paddingTop = "0";
-const slideEvent = setInterval(function() {
+slideListImg[0].style.boxShadow = "5px 5px 10px rgba(0, 0, 0, 0.3)";
+
+
+function slideInterval() {
     slideList.style.left = "-550px";
+    slideListImg[1].style.boxShadow = "5px 5px 10px rgba(0, 0, 0, 0.3)";
     slideListImg[1].style.width = "500px";
     slideListImg[1].style.paddingTop = "0";
+    
+    if(stickMove < 110) {
+        stickMove += stickMoveAvg;
+    } else {
+        stickMove = 0;
+    }
+
+    slideStick.style.left = stickMove + "px";
+    
+    
+    slideBg.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(" + slideListImg[1].firstChild.src + ")"
     setTimeout(function() {
         slideListImg[0].style.paddingTop = "100px";
         slideListImg[0].style.width = "200px";
+        slideListImg[0].style.boxShadow = "none";
         slideList.appendChild(slideListImg[0]);
         slideList.style.transition = "none";
         slideList.style.left = "0";
     }, 1000);
     slideList.style.transition = "all 1s";
-},3000);
+}
+
+
+let slideEvent = setInterval(slideInterval, 3000);
+
+let slideToggle = true;
+slideBtn.addEventListener("click", function() {
+    if(slideToggle) {
+        clearInterval(slideEvent);
+        slideToggle = false;
+    } else {
+        slideEvent = setInterval(slideInterval, 3000);
+        slideToggle = true;
+    }
+    
+}, false);
